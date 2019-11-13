5594fe791e0eacf07e077d2241fcb6ae0c4dbd7c
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-11-13 10:01:12 +0800

    added comments for clarity

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 //importing just one of the methods from react-dom i.e. the {render} method
 import { render } from "react-dom";
+//import the component from the components folder
 import StorePicker from "./components/StorePicker";
 
 //render the component inside of #main
