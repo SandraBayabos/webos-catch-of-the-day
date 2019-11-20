b6280274f261537cd98434c911fa2a52e2943a3b
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-11-20 14:55:33 +0800

    edited Headers.js to create stateless functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,47 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            {" "}
-            <span className="of"> of</span>
-            <span className="the">the</span>
-          </span>{" "}
-          Day
-        </h1>
-        {/* tagline is a prop passed down from App.js */}
-        <h3 className="tagline">
-          <span>{this.props.tagline}</span>
-        </h3>
-      </header>
-    );
-  }
-}
+// instead of a class Header we create a stateless functional component
+
+//pass the props in directly (props) so no need to do this.props.propName, just to props.propName
+
+const Header = props => {
+  return (
+    <header className="top">
+      <h1>
+        Catch
+        <span className="ofThe">
+          <span className="of"> of</span>
+          <span className="the">the</span>
+        </span>
+        Day
+      </h1>
+      {/* tagline is a prop passed down from App.js */}
+      <h3 className="tagline">
+        <span>{props.tagline}</span>
+      </h3>
+    </header>
+  );
+};
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header className="top">
+//         <h1>
+//           Catch
+//           <span className="ofThe">
+//             <span className="of"> of</span>
+//             <span className="the">the</span>
+//           </span>
+//           Day
+//         </h1>
+//         {/* tagline is a prop passed down from App.js */}
+//         <h3 className="tagline">
+//           <span>{this.props.tagline}</span>
+//         </h3>
+//       </header>
+//     );
+//   }
+// }
 
 export default Header;
